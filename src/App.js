9ecedd5d45ff4b293f419b8fd7fd7aa360c574c2
KocsiAndras83 <66732777+KocsiAndras83@users.noreply.github.com>
2021-04-27 20:54:47 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css'
+import React, { useState, useEffect} from 'react'
+import Client from './Components/Client'
+import Loading from './Components/Loading'
 
 const App = () => {
 
+  const [data, setData] = useState(null)
+  const [loader, setLoader] = useState(false)
+  const [showClient, setShowClient] = useState(false)
+  const [buttonClicked, setButtonClicked] = useState(false)
+
+  const fetchData = () => {
+    fetch("https://api/clients?search=AMIT-A-FELHASZNÁLÓ-ÍRT")
+    .then((response) => response.json())
+    .then((data) => {setData(data)})
+  }
+
   return (
     <div className="App">
-      Todo...
+      <h1>Veterinarian admin - clients</h1>
+      <input placeholder='Search...'></input>
+      <button onClick={setButtonClicked(!buttonClicked)}>Search</button>
+      
+      {buttonClicked && (
+       <div>
+         {data.map((client, i) => <Client client={client} key={i} />)}
+       </div>)}
+      <Loading />
     </div>
   )
 }
